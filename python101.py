58db62de7e573f02b72a3e35a9227006f63d090b
Rattaporn <68935549+Kggzzz@users.noreply.github.com>
2022-03-30 14:09:34 +0700

    เรียบร้อย

---
+++ python101.py
@@ -0,0 +1,32 @@
+import argparse #สำหรับ รับ input จากภายนอก
+import subprocess #สำหรับรัน terminal command 
+
+#import flask # สำหรับทำ web app และ web service api
+
+
+def print_other():
+    print('something else')
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='test program to learn about argparse ')
+    parser.add_argument(
+        'm',
+        type=int,
+        help='value of positional argument')
+    
+    parser.add_argument(
+        '--x',
+        type=int,
+        help='value of x')
+    
+    parser.add_argument(
+        '--yval',
+        type=int,
+        default=3,
+        help='value of y')
+    args = parser.parse_args()
+
+    x = args.x
+    y = args.yval
+    print(f'M = {args.m}')
+    print(f'calculate {x} x {y} = {x*y}')
